ad1665016e4a573519ee68322a4c396eb1e38efd
Madusha dilshan <143088487+madushabangamuwage2001@users.noreply.github.com>
2024-07-07 10:18:26 +0530

    full

---
+++ app/src/main/java/com/example/taskapp/Logoscreen.kt
@@ -0,0 +1,21 @@
+package com.example.taskapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class Logoscreen : AppCompatActivity() {
+    private val handler = Handler()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_logoscreen2)
+
+        handler.postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 5000L)
+    }
+}
